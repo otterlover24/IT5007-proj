cae351b06d040c85334b4a5c1fa7c726df9cb5f4
otterlover24 <otterlover24@gmail.com>
2022-04-05 13:13:23 +0000

    Cleans up residual components from reference.

--- client/src/components/watchlist.component.js
+++ client/src/components/watchlist.component.js
@@ -3,45 +3,20 @@ import React, { useState, useEffect } from "react";
 import "../App.css";
 import Axios from "axios";
 import "../components/component.css";
-import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import ErrorModal from "../components/error-modal.component";
-import EditModal from "../components/edit-modal.component";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrashAlt, faEdit } from "@fortawesome/free-solid-svg-icons";
-
-import { Doughnut } from "react-chartjs-2";
 
 import {
   Container,
-  Dropdown,
-  ButtonGroup,
-  DropdownButton,
-  Button,
   Col,
   Row,
   Table,
 } from "react-bootstrap";
 
 export default function Watchlist() {
-  const [transactionDate, setTransactionDate] = useState(new Date());
-  const [transactions, setTransactions] = useState([]);
-
-  const [transactionTitle, setTransactionTitle] = useState();
-  const [transactionAmount, setTransactionAmount] = useState();
-
-  const [transactionType, setTransactionType] = useState();
 
   const [tickerToAdd, setTickerToAdd] = useState();
   const [tickerToDelete, setTickerToDelete] = useState();
   const [watchlist, setWatchlist] = useState();
-
-  const [error, setError] = useState();
-  const [editModalShow, setEditModalShow] = useState(false);
-
-  const [errorModalShow, setErrorModalShow] = useState(false);
-
-  const [transactionToEdit, setTransactionToEdit] = useState([]);
   
   useEffect(() => {
     const checkLoggedIn = async () => {
@@ -62,7 +37,6 @@ export default function Watchlist() {
     };
     checkLoggedIn();
 
-    readTransactions();
     displayWatchlist();
     console.log("test");
   }, []);
@@ -87,19 +61,7 @@ export default function Watchlist() {
     console.log(watchlist);
   }, [watchlist])
 
-  const readTransactions = async () => {
-    setEditModalShow(false);
-    await Axios({
-      method: "get",
-      url: "http://localhost:5000/api/protected/income//getHistory",
-      headers: {
-        Authorization: localStorage.getItem("jwt"),
-      },
-    }).then(res => {
-      setTransactions(res.data);
-      // calculateNetworth(res.data);
-    });
-  };
+
   
   const onAddTickerSubmit = async e => {
     try {
@@ -119,8 +81,7 @@ export default function Watchlist() {
         console.log(`Sent ${res} to /api/protected/watchlist/addTicker`);
       });
     } catch (err) {
-      setError(err.response.data.Error);
-      setErrorModalShow(true);
+      console.error(err);
     }
   };
 
@@ -142,76 +103,12 @@ export default function Watchlist() {
         console.log(`Sent ${res} to /api/protected/watchlist/deleteTicker`);
       });
     } catch (err) {
-      setError(err.response.data.Error);
-      setErrorModalShow(true);
+      console.error(err);
     }
   };
-
-  const onChangeDate = date => {
-    setTransactionDate(date);
-  };
-  const deletetransaction = async id => {
-    await Axios({
-      method: "delete",
-      url: "http://localhost:5000/api/protected/income/deleteTransaction/" + id,
-      headers: {
-        Authorization: localStorage.getItem("jwt"),
-      },
-    }).then(res => {
-      setTransactions(res.data);
-    });
-  };
-  const editTransaction = async transaction => {
-    setTransactionToEdit(transaction);
-    setEditModalShow(true);
-  };
-  const Transactions = props => {
-    let amount = props.transaction.transactionAmount;
-    if (props.transaction.transactionType === "EXPENSE") {
-      amount = "-$" + amount;
-    } else {
-      amount = "$" + amount;
-    }
-    return (
-      <tbody>
-        <tr>
-          <td key={props.transaction._id}>
-            {props.transaction.transactionTitle}
-          </td>
-          <td>{props.transaction.transactionType}</td>
-          <td>{amount}</td>
-          <td>{props.transaction.transactionDate.substring(0, 10)}</td>
-          <td className="test">
-            <Button
-              id="edit-button"
-              onClick={() => editTransaction(props.transaction)}
-            >
-              <FontAwesomeIcon icon={faEdit} />
-            </Button>
-            <Button
-              id="delete-button"
-              onClick={() => deletetransaction(props.transaction._id)}
-            >
-              <FontAwesomeIcon icon={faTrashAlt} />
-            </Button>{" "}
-          </td>
-        </tr>
-      </tbody>
-    );
-  };
-
+  
   return (
     <Container>
-      <EditModal
-        show={editModalShow}
-        onHide={() => readTransactions()}
-        transaction={transactionToEdit}
-      />
-      <ErrorModal
-        show={errorModalShow}
-        onHide={() => setErrorModalShow(false)}
-        error={error}
-      />
       <Row>
         <Col xs="12" md="6">
           <div className="card expense-input-card">
@@ -263,15 +160,11 @@ export default function Watchlist() {
 
       <Row>
         <Col xs="12">
-          <Table className="watchlisttable" striped bordered hover responsive>
+          <Table className="watchlistTable" striped bordered hover responsive>
             <thead>
               <tr>
                 <th>Ticker Symbol</th>
                 <th>Adjusted Monthly Closing Price</th>
-
-                <th>Amount</th>
-                <th>Date</th>
-                <th>Edit/Delete</th>
               </tr>
             </thead>
             
@@ -289,29 +182,6 @@ export default function Watchlist() {
         </Col>
       </Row>
 
-      <Row>
-        <Col xs="12">
-          <Table className="income-table" striped bordered hover responsive>
-            <thead>
-              <tr>
-                <th>Transaction Name</th>
-                <th>Transaction Type</th>
-
-                <th>Amount</th>
-                <th>Date</th>
-                <th>Edit/Delete</th>
-              </tr>
-            </thead>
-
-            {transactions.map(currentTransaction => (
-              <Transactions
-                transaction={currentTransaction}
-                key={currentTransaction._id}
-              />
-            ))}
-          </Table>
-        </Col>
-      </Row>
     </Container>
   );
 }
