67259529b146adbe1b36a37b9fd6b72bbf856ed8
otterlover24 <otterlover24@gmail.com>
2022-04-21 06:06:11 +0000

    In trade.router.js, remove routes for edit and delete.

--- backend/routes/trade.router.js
+++ backend/routes/trade.router.js
@@ -64,33 +64,5 @@ router.post( '/submitTrade', async ( req, res ) => {
 
 } );
 
-router.delete( '/deleteTransaction/:id', async ( req, res ) => {
-  const transactionToDelete = await Transaction.findOne( { _id: req.params.id } );
-  if ( !transactionToDelete ) {
-    return res.status( 400 ).json( { Error: "Transaction not found" } );
-  }
-
-
-  const deletedTransaction = await Transaction.findByIdAndDelete( transactionToDelete._id );
-  const transactions = await Transaction.find( { userId: req.user._id } );
-  return res.json( transactions );
-} );
-
-
-
-router.put( '/editTransaction/:id', async ( req, res ) => {
-  let { transactionTitle, transactionType, transactionAmount, transactionDate } = req.body;
-  if ( !transactionTitle || !transactionType || !transactionAmount || !transactionDate ) {
-    return res.status( 400 ).json( { Error: "Not all fields have been entered" } );
-  }
-  // console.log(_id+" "+transactionTitle+" "+transactionAmount+" "+transactionDate+" "+transactionType);
-
-  await Transaction.findByIdAndUpdate( { _id: req.params.id }, req.body
-
-  )
-    .then( transaction => res.json( transaction ) )
-    .catch( err => res.status( 400 ).json( { Error: err } ) );
-
-} );
 
 module.exports = router;
