8aaf26eb3768652dc38491d8daa336f6917a0896
otterlover24 <otterlover24@gmail.com>
2022-04-23 02:42:49 +0000

    After revert to client side set state.

--- client/src/components/portfolio.component.js
+++ client/src/components/portfolio.component.js
@@ -11,26 +11,32 @@ import {
 } from "react-bootstrap";
 import { Line } from "react-chartjs-2";
 
-class Portfolio extends React.Component {
+export default function Portfolio( props ) {
+  const [ netWorthData, setNetWorthData ] = useState( {
+    labels: [ "2014-06", "2014-07", "2014-08", "2014-09" ],
+    datasets: [
+      {
+        label: "Net Worth",
+        data: [ 10000, 20000, 40000, 35000 ],
+        lineTension: 0,
+      }
+    ]
+  } );
 
-  constructor( props ) {
-    super( props );
-    this.state = {
-      trades: [],
-      viewingMonthHoldings: {},
-    };
-  }
+  const [ trades, setTrades ] = useState( [] );
+  const initialViewingMonthHoldings = {};
+  const [ viewingMonthHoldings, setViewingMonthHoldings ] = useState( initialViewingMonthHoldings );
 
-  async componentDidMount() {
-    console.log( "portfolio.component.js: executing componentDidMount" );
-    this.checkLoggedIn();
-    await this.displayTrades();
-    console.log( "In componentDidMount, this.state.trade: ", this.state.trade );
-  }
-
-
-
-
+  useEffect(
+    () => {
+      console.log( "portfolio.component.js useEffect []" );
+      console.log( "portfolio.component props.viewingMonth: ", props.viewingMonth );
+      console.log( "portfolio.component props.latestMonth: ", props.latestMonth );
+      checkLoggedIn();
+      displayTrades();
+    },
+    []
+  );
 
   // useEffect(
   //   () => {
@@ -117,22 +123,72 @@ class Portfolio extends React.Component {
     } ).then( res => {
       console.log( "displayTrades received res.data from server: \n", res.data );
       setTrades( res.data.trades );
-      setViewingMonthHoldings(res.data.holdings);
-    } );
 
-    /* Get market price for viewingMonthHoldings */
+    } );
 
   };
 
-  render() {
-    return (
-      <Container>
+  return (
+    <Container>
+      <Row>
+        <Col xs="12" md="6">
+          <div className="card expense-input-card">
+            <div className="card-body">
+              <h5>Net Worth Chart</h5>
+              <div style={ { width: 500 } }>
+                <Line data={ netWorthData } />
+              </div>
 
-        <Row>
-          <h5>Current Holdings</h5>
-          <Col xs="12">
-            <Table id="holdingsTable" striped bordered hover responsive>
-              <thead>
+
+
+            </div>
+          </div>
+        </Col>
+      </Row>
+
+      <Row>
+        <h5>Current Holdings</h5>
+        <Col xs="12">
+          <Table id="holdingsTable" striped bordered hover responsive>
+            <thead>
+              <tr>
+                <th>Ticker</th>
+                <th>Quantity</th>
+                <th>Average Purchase Price</th>
+                <th>Current Market Price</th>
+                <th>Profit (Loss)</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              <tr>
+                <td>AAPL</td>
+                <td>20,000</td>
+                <td>$134.00</td>
+                <td>$174.00</td>
+                <td>$800,000</td>
+              </tr>
+            </tbody>
+
+          </Table>
+        </Col>
+      </Row>
+
+      <Row>
+        <h5>Transaction History</h5>
+        <Col xs="12">
+          <Table id="transactionHistoryTable" striped bordered hover responsive>
+            <thead>
+              <tr>
+                <th>Date</th>
+                <th>Ticker</th>
+                <th>Direction</th>
+                <th>Amount</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              { trades ? trades.map( trade => (
                 <tr>
                   <th>Ticker</th>
                   <th>Quantity</th>
