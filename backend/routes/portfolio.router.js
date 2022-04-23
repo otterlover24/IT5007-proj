3a723ae162f86a9f3259db35c0d0ecd015a710ef
otterlover24 <otterlover24@gmail.com>
2022-04-23 04:14:17 +0000

    For ticker US-DOLLAR, server returns quote 1.0

--- backend/routes/portfolio.router.js
+++ backend/routes/portfolio.router.js
@@ -32,31 +32,38 @@ router.post( '/getTrades', async ( req, res ) => {
       console.log( "holdings: ", holdings );
 
       /* Get market price as at req.user.viewingMonth */
-      let apiUrl = 'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol=' +
-        trade.tickerSymbol +
-        '&datatype=json' +
-        '&apikey=' +
-        process.env.VANTAGE_KEY;
+      let quote;
+      if ( trade.tickerSymbol === "US-DOLLAR" ) {
+        quote = "1.0";
+      }
+      if ( trade.tickerSymbol !== "US-DOLLAR" ) {
+        let apiUrl = 'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol=' +
+          trade.tickerSymbol +
+          '&datatype=json' +
+          '&apikey=' +
+          process.env.VANTAGE_KEY;
 
 
 
-      let quote = await axios
-        .get( apiUrl )
-        .then( apiRes => {
-          for ( const property in apiRes.data[ 'Monthly Adjusted Time Series' ] ) {
-            console.log( "property: ", property );
-            console.log( "property.slice(0, 7): ", property.slice( 0, 7 ) );
-            console.log( "req.user.latestMonth: ", req.user.latestMonth );
-            if ( property.slice( 0, 7 ) === req.user.latestMonth ) {
-              return apiRes.data[ 'Monthly Adjusted Time Series' ][ property ][ '5. adjusted close' ];
+        quote = await axios
+          .get( apiUrl )
+          .then( apiRes => {
+            for ( const property in apiRes.data[ 'Monthly Adjusted Time Series' ] ) {
+              console.log( "property: ", property );
+              console.log( "property.slice(0, 7): ", property.slice( 0, 7 ) );
+              console.log( "req.user.latestMonth: ", req.user.latestMonth );
+              if ( property.slice( 0, 7 ) === req.user.latestMonth ) {
+                return apiRes.data[ 'Monthly Adjusted Time Series' ][ property ][ '5. adjusted close' ];
+              }
             }
-          }
-          return;
-        } );
+            return;
+          } );
+      }
+
 
       let directionSign = ( trade.direction === "BUY" ) ? 1 : -1;
       if ( trade.tickerSymbol in holdings ) {
-        console.log("debug holdings[trade.tickerSymbol]: ", holdings[trade.tickerSymbol]);
+        console.log( "debug holdings[trade.tickerSymbol]: ", holdings[ trade.tickerSymbol ] );
         holdings[ trade.tickerSymbol ][ "quantity" ] += directionSign * trade.quantity;
       }
       if ( !( trade.tickerSymbol in holdings ) ) {
